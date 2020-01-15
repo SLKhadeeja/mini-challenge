978c9b287f137db28504d50ed79986c38946ce93
Khadija Ladan <khadijaladan67@gmail.com>
2020-01-15 21:36:56 +0100

    completed challenge 1

--- challenge.js
+++ challenge.js
@@ -1,6 +1,21 @@
 /* This function accepts a string containing multiple words as input
    You're expected to find and return the length of the longest word in the string */
    const findLongestWordLength = (str) => {
+     var sentence = str.split(' ');
+     var lOfWords = []
+     largest = 0;
+     for (var i=0; i<sentence.length; i++){
+       lOfWords[i] = sentence[i].length;
+     }
+     //console.log(lOfWords);
+     for (var i=0; i<lOfWords.length; i++){
+       if (largest < lOfWords[i]){
+         largest = lOfWords[i]
+       }
+     }
+     largestWordIndex = lOfWords.indexOf(largest);
+
+     return(sentence[largestWordIndex])
     // Add your logic here
   }
   
