46b21a1d7fd7a548e973a84fd5abaa1137e439c1
Khadija Ladan <khadijaladan67@gmail.com>
2020-01-19 10:11:30 +0100

    Added conditions

--- challenge.js
+++ challenge.js
@@ -1,31 +1,35 @@
 /* This function accepts a string containing multiple words as input
    You're expected to find and return the length of the longest word in the string */
    const findLongestWordLength = (str) => {
-     var sentence = str.split(' ');
-     var lOfWords = []
-     largest = 0;
-     for (var i=0; i<sentence.length; i++){
-       lOfWords[i] = sentence[i].length;
-     }
-     //console.log(lOfWords);
-     for (var i=0; i<lOfWords.length; i++){
-       if (largest < lOfWords[i]){
-         largest = lOfWords[i]
-       }
-     }
-     largestWordIndex = lOfWords.indexOf(largest);
+     if (typeof str == "string"){
+        var sentence = str.split(' ');
+        var lOfWords = []
+        largest = 0;
+        for (var i=0; i<sentence.length; i++){
+          lOfWords[i] = sentence[i].length;
+        }
+        //console.log(lOfWords);
+        for (var i=0; i<lOfWords.length; i++){
+          if (largest < lOfWords[i]){
+            largest = lOfWords[i]
+          }
+        }
+        largestWordIndex = lOfWords.indexOf(largest);
 
-     return(sentence[largestWordIndex])
-    // Add your logic here
+        return(sentence[largestWordIndex])
+      }
+      // Add your logic here
   }
   
   // Return an array consisting of the largest number from each provided sub-array
   const findLargestOfArrays = (arrays) => {
-    return arrays.map((subArrays) => {
-      subArrays.reduce((previousLargestNumber, currentLargestNumber) => {
-        (currentLargestNumber > previousLargestNumber) ? currentLargestNumber : previousLargestNumber;
-      }, 0);
-    });
+    if (Array.isArray(arrays)){
+      return arrays.map((subArrays) => {
+        subArrays.reduce((previousLargestNumber, currentLargestNumber) => {
+          (currentLargestNumber > previousLargestNumber) ? currentLargestNumber : previousLargestNumber;
+        }, 0);
+      });
+    }
     // Add your logic here
   }
   
